c2e57377f006209064ad50bb33b29eb706644f57
Igor Goldvekht <goldvekht.igor@gmail.com>
2020-02-27 00:52:39 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -62,7 +62,7 @@ downloadAndInstallDotfiles() {
 }
 
 installNodejs() {
-  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
   nvm install node
   nvm use node
 }
