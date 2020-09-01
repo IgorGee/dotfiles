9fa26aed184496b77378ba9c833eae99e07a3bf4
Igor Goldvekht <goldvekht.igor@gmail.com>
2020-09-01 08:38:53 +0000

    Fix manual downloads

--- scripts/manualDownloads.sh
+++ scripts/manualDownloads.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env zsh
 
-curl -LO https://github.com/EliverLara/Sweet/releases/download/v1.10.5/Sweet-Dark.zip
-x Sweet-Dark.zip
-mv Sweet-Dark/Sweet-Dark "$HOME/dotfiles/xdgFileStructure/.local/share/theme/"
-rm Sweet-Dark.zip
+# curl -LO https://github.com/EliverLara/Sweet/releases/download/v1.10.5/Sweet-Dark.zip
+# unzip Sweet-Dark.zip -d "$HOME/dotfiles/xdgFileStructure/.local/share/themes"
+# rm Sweet-Dark.zip
 
-cd "$HOME/programs/yay" || exit
-makepkg -si
-yay -Syu libxft-bgra
+# cd "$HOME/programs/yay" || exit
+# makepkg -si
+# yay -Syu libxft-bgra
 # Might be necessary
 # gpg --keyserver "hkp://ipv4.pool.sks-keyservers.net:11371" --recv-keys 4A193C06D35E7C670FA4EF0BA2FB9E081F2D130E
+
+# cd "$HOME/repos/personal"
+# git clone --recursive git@github.com:IgorGee/pdotfiles.git
+# cd pdotfiles
+# ./downloadRepos.zsh
