93cb900c871e83cfda69c148eaa9b52d8f32fbde
Igor Goldvekht <goldvekht.igor@gmail.com>
2020-09-06 03:31:36 +0000

    Add FZF default options

--- xdgFileStructure/.config/nixpkgs/modules/zsh/default.nix
+++ xdgFileStructure/.config/nixpkgs/modules/zsh/default.nix
@@ -43,6 +43,20 @@ in {
 
     # FZF
     FZF_DEFAULT_COMMAND = "rg --files --hidden --follow --glob '!.git'"; # Using rg for speed
+    FZF_DEFAULT_OPTS="
+      --layout=reverse
+      --info=inline
+      --height=80%
+      --multi
+      --preview-window=:hidden
+      --preview '([[ -f {} ]] && (bat --style=numbers --color=always {} || cat {})) || ([[ -d {} ]] && (tree -C {} | less)) || echo {} 2> /dev/null | head -200'
+      --color='hl:148,hl+:154,pointer:032,marker:010,bg+:237,gutter:008'
+      --bind '?:toggle-preview'
+      --bind 'ctrl-a:select-all'
+      --bind 'ctrl-y:execute-silent(echo {+} | pbcopy)'
+      --bind 'ctrl-e:execute(echo {+} | xargs -o vim)'
+      --bind 'ctrl-v:execute(code {+})'
+    ";
 
     # Something is messing with my locale :(
     LANG="en_US.UTF-8";
