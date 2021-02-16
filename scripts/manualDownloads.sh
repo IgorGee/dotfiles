8d0e6ebba79a038fe98bd5ef4fd33a60ae328057
Igor Goldvekht <goldvekht.igor@gmail.com>
2021-02-16 19:45:40 +0000

    Uncomment manual downloads

--- scripts/manualDownloads.sh
+++ scripts/manualDownloads.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env zsh
 
-# curl -LO https://github.com/EliverLara/Sweet/releases/download/v1.10.5/Sweet-Dark.zip
-# unzip Sweet-Dark.zip -d "$HOME/dotfiles/xdgFileStructure/.local/share/themes"
-# rm Sweet-Dark.zip
+curl -LO https://github.com/EliverLara/Sweet/releases/download/v1.10.5/Sweet-Dark.zip
+unzip Sweet-Dark.zip -d "$HOME/dotfiles/xdgFileStructure/.local/share/themes"
+rm Sweet-Dark.zip
 
-# cd "$HOME/programs/yay" || exit
-# makepkg -si
-# yay -Syu libxft-bgra
-# Might be necessary
-# gpg --keyserver "hkp://ipv4.pool.sks-keyservers.net:11371" --recv-keys 4A193C06D35E7C670FA4EF0BA2FB9E081F2D130E
+cd "$HOME/programs/yay" || exit
+makepkg -si
+yay -Syu libxft-bgra
+Might be necessary
+gpg --keyserver "hkp://ipv4.pool.sks-keyservers.net:11371" --recv-keys 4A193C06D35E7C670FA4EF0BA2FB9E081F2D130E
 
-# cd "$HOME/repos/personal"
-# git clone --recursive git@github.com:IgorGee/pdotfiles.git
-# cd pdotfiles
-# ./downloadRepos.zsh
+cd "$HOME/programs/st" || exit
+sudo make install
+
+cd "$HOME/repos/personal" || exit
+git clone --recursive git@github.com:IgorGee/pdotfiles.git
+cd pdotfiles || exit
+./downloadRepos.zsh
