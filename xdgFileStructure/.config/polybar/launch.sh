91d97a6c85e65de059e98b41ab45d524a5418694
Igor Goldvekht <goldvekht.igor@gmail.com>
2021-05-09 03:37:07 -0400

    Update scripts for single monitor usage

--- xdgFileStructure/.config/polybar/launch.sh
+++ xdgFileStructure/.config/polybar/launch.sh
@@ -6,9 +6,14 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Multimonitor support
 if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1 | tail -n +2); do
-    MONITOR=$m polybar --reload bar &
-  done
+  # if there's only one monitor present, then don't do anything special
+  if [[ $(xrandr --query | grep " connected" | cut -d" " -f1 | wc -l) -eq 1 ]]; then
+    polybar --reload bar &
+  else
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1 | tail -n +2); do
+      MONITOR=$m polybar --reload bar &
+    done
+  fi
 else
   polybar --reload bar &
 fi
