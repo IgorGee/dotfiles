d8bccd4ad4f49e595c6536435ff10685d183f76a
Igor Goldvekht <goldvekht.igor@gmail.com>
2021-05-21 06:06:01 -0400

    Set up systemd services after install

--- scripts/installNix.sh
+++ scripts/installNix.sh
@@ -1,6 +1,15 @@
+#!/usr/bin/env bash
+
+nixProfileDir="/home/igor/.nix-profile"
+
 curl -L https://nixos.org/nix/install | sh
-source /home/igor/.nix-profile/etc/profile.d/nix.sh
+source "$nixProfileDir/etc/profile.d/nix.sh"
 nix-channel --add https://github.com/rycee/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
 nix-shell '<home-manager>' -A install
 home-manager switch
+
+systemDDir="$nixProfileDir/etc/systemd/system/*"
+for filename in $systemDDir; do
+  sudo systemctl link "$(readlink "$filename")"
+done
