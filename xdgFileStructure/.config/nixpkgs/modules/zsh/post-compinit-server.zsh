65c0f8d480a784cc8dc5b6472cdd452ee38ffd71
Igor Goldvekht <goldvekht.igor@gmail.com>
2021-08-10 12:42:07 +0000

    Add namespace deletion fix

--- xdgFileStructure/.config/nixpkgs/modules/zsh/post-compinit-server.zsh
+++ xdgFileStructure/.config/nixpkgs/modules/zsh/post-compinit-server.zsh
@@ -1,2 +1,12 @@
 zshDirPath=$HOME/dotfiles/xdgFileStructure/.config/nixpkgs/modules/zsh
 source $zshDirPath/post-compinit-common.zsh
+
+# https://stackoverflow.com/a/59667608
+function k8s-remove-stuck-ns() {
+  local namespace=$1
+
+  kubectl get namespace "$namespace" -o json \
+  | tr -d "\n" | sed "s/\"finalizers\": \[[^]]\+\]/\"finalizers\": []/" \
+  | kubectl replace --raw /api/v1/namespaces/"$namespace"/finalize -f -
+
+}
