5d2af38bf6b3f2c320e970d1d5544665d84c0b9c
Igor Goldvekht <goldvekht.igor@gmail.com>
2022-01-27 03:19:13 +0000

    Fix NIX_PATH

--- scripts/installNix.sh
+++ scripts/installNix.sh
@@ -4,7 +4,7 @@ nixProfileDir="/home/igor/.nix-profile"
 
 curl -L https://nixos.org/nix/install | sh
 source "$nixProfileDir/etc/profile.d/nix.sh"
-export NIX_PATH=$HOME/.nix-defexpr/channels
+export NIX_PATH=$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels${NIX_PATH:+:$NIX_PATH}
 nix-channel --add https://github.com/rycee/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
 nix-shell '<home-manager>' -A install
